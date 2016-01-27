d2325cf6ebb690732fd422e6b8d5c93b1fba1181
Max Harlow <maxharlow@gmail.com>
2016-01-27 13:44:18 +0000

    Make it all ES2015y

--- who-said-that.js
+++ who-said-that.js
@@ -1,53 +1,49 @@
-var highland = require('highland')
-var request = require('request')
-var csvWriter = require('csv-write-stream')
-var fs = require('fs')
-var config = require('./config')
+const Highland = require('highland')
+const Request = require('request')
+const CSVWriter = require('csv-write-stream')
+const FS = require('fs')
+const Config = require('./config')
 
 function queryTwitterSearch(search) {
     return {
         q: search,
-        result_type: config.resultType,
+        result_type: Config.resultType,
         count: 100
     }
 }
 
 function createTwitterSearch(parser) {
     return function query(qs, callback) {
-        var sleep = 12 * 1000 // 12 seconds
-        var params = {
+        const sleep = 12 * 1000 // 12 seconds
+        const params = {
             url: 'https://api.twitter.com/1.1/search/tweets.json',
             qs: qs,
             oauth: {
-                consumer_key: config.twitter.consumerKey,
-                consumer_secret: config.twitter.consumerSecret,
-                token: config.twitter.accessTokenKey,
-                token_secret: config.twitter.accessTokenSecret
+                consumer_key: Config.twitter.consumerKey,
+                consumer_secret: Config.twitter.consumerSecret,
+                token: Config.twitter.accessTokenKey,
+                token_secret: Config.twitter.accessTokenSecret
             }
         }
-        request.get(params, function (error, response) {
-            if (error || response === undefined || response.statusCode !== 200) {
+        Request.get(params, (e, response) => {
+            if (e || response === undefined || response.statusCode !== 200) {
                 console.log('Error! Sleeping before retrying...')
-                setTimeout(function () { query(qs, callback) }, sleep)
+                setTimeout(() => query(qs, callback), sleep)
                 return
             }
-            var body = JSON.parse(response.body)
-            var data = parser(qs, body)
-            if (body.search_metadata.next_results) {
-                setTimeout(function () {
-                    qs.max_id = body.search_metadata.next_results.match(/max_id=(.*)&q/i)[1]
-                    query(qs, function (_, dataNext) {
-                        callback(null, data.concat(dataNext))
-                    })
-                }, sleep)
-            }
+            const body = JSON.parse(response.body)
+            const data = parser(qs, body)
+            if (body.search_metadata.next_results) setTimeout(() => {
+                qs.max_id = body.search_metadata.next_results.match(/max_id=(.*)&q/i)[1]
+                query(qs, (_, dataNext) => callback(null, data.concat(dataNext)))
+            }, sleep)
             else callback(null, data)
         })
     }
 }
 
-var doTwitterSearch = createTwitterSearch(function (qs, response) {
-    return response.statuses.map(function (tweet) {
+const doTwitterSearch = createTwitterSearch((qs, response) => {
+    return response.statuses.map(tweet => {
         return {
             url: 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str,
             byUser: tweet.user.name,
@@ -60,9 +56,9 @@ var doTwitterSearch = createTwitterSearch(function (qs, response) {
     })
 })
 
-highland(config.searchTerms)
+Highland(Config.searchTerms)
     .map(queryTwitterSearch)
-    .flatMap(highland.wrapCallback(doTwitterSearch))
+    .flatMap(Highland.wrapCallback(doTwitterSearch))
     .flatten()
-    .pipe(csvWriter())
-    .pipe(fs.createWriteStream('results.csv'))
+    .pipe(CSVWriter())
+    .pipe(FS.createWriteStream('results.csv'))
